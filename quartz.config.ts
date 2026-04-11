7595a4c92547eb509db45085d7c97028eb288dbc
great小创 <2920174611@qq.com>
2026-04-11 22:46:40 +0800

    set pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "秦幼卓的笔记",
+    pageTitle: "QYouz的笔记",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
